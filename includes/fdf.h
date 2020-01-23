17714a75888f92f4a07e2674ad5c91e3595199d9
rcenamor <rcenamor@c1r1p2.hive.fi>
2020-01-23 13:35:20 +0200

    Reorganized the files and changed Makefile

---
+++ includes/fdf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcenamor <rcenamor@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 13:15:10 by rcenamor          #+#    #+#             */
+/*   Updated: 2020/01/23 13:18:43 by rcenamor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+# include "mlx.h"
+
+# include <stdio.h>
+
+typedef struct	s_fdf
+{
+	void		*mlx;
+	void		*win;
+}				t_fdf;
+
+#endif
