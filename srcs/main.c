4e2e8e47b92272226e4e5f33c1d8dceefdf0155e
kipVk <rcenamor@student.hive.fi>
2020-01-24 11:15:20 +0200

    Draws lines but not straight

--- srcs/main.c
+++ srcs/main.c
@@ -52,17 +52,25 @@ void draw_line(int x0, int y0, int x1, int y1, void *mlx_ptr, void *win_ptr)
     int err;
     int e2;
  
-    dx = ft_abs(x1-x0);
-    dy = ft_abs(y1-y0); 
-    err = (dx>dy ? dx : -dy)/2;
-    sx = x0<x1 ? 1 : -1;
-    sy = y0<y1 ? 1 : -1; 
+    dx = ft_abs(x1 - x0);
+    dy = ft_abs(y1 - y0); 
+    err = (dx > dy ? dx : -dy) / 2;
+    sx = x0 < x1 ? 1 : -1;
+    sy = y0 < y1 ? 1 : -1; 
     while(x0 != x1 && y0 != y1)
     {
         mlx_pixel_put(mlx_ptr, win_ptr, x0, y0, 0x000000);
         e2 = err;
-        if (e2 >-dx) { err -= dy; x0 += sx; }
-        if (e2 < dy) { err += dx; y0 += sy; }
+        if (e2 > -dx)
+        {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dy)
+        {
+            err += dx;
+            y0 += sy;
+        }
     }
 }
 
@@ -74,7 +82,7 @@ int main()
     mlx_ptr = mlx_init();
     win_ptr = mlx_new_window(mlx_ptr, 818, 500, "FDF");
     paint_background(mlx_ptr, win_ptr);
-    draw_line(20, 20, 520, 120, mlx_ptr, win_ptr);
+    draw_line(20, 20, 20, 100, mlx_ptr, win_ptr);
     //mlx_key_hook(win_ptr, key_press, mlx_ptr);
     mlx_hook(win_ptr, 2, 0, key_press, mlx_ptr);
     mlx_hook(win_ptr, 4, 0, mouse_press, mlx_ptr);
