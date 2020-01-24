4bf3032f33c5f2ccf9ef4cb9930b25ba8fe1ed32
kipVk <rcenamor@student.hive.fi>
2020-01-24 15:36:42 +0200

    Fixed spaces on ft_sqrt.c

--- libft/sources/ft_sqrt.c
+++ libft/sources/ft_sqrt.c
@@ -5,32 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcenamor <rcenamor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/24 11:48:44 by rcenamor          #+#    #+#             */
-/*   Updated: 2020/01/24 11:54:31 by rcenamor         ###   ########.fr       */
+/*   Created: 2020/01/24 15:31:12 by rcenamor          #+#    #+#             */
+/*   Updated: 2020/01/24 15:34:32 by rcenamor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_sqrt(int nb)
+int		ft_sqrt(int nb)
 {
-    int sq;
-    int count;
+	int		sq;
+	int		count;
 
-    count = 1;
-    sq = 0;
-    if (nb == 0)
-        sq = 0;
-    else if (nb == 1)
-        sq = 1;
-    else
-    {
-        while (++count < nb)
-        {
-            if ((count * count) == nb)
-                sq = count;
-        }
-    }
-    ft_putnbr(sq);
-    return (sq);
+	count = 1;
+	sq = 0;
+	if (nb == 0)
+		sq = 0;
+	else if (nb == 1)
+		sq = 1;
+	else
+	{
+		while (++count < nb)
+		{
+			if ((count * count) == nb)
+				sq = count;
+		}
+	}
+	ft_putnbr(sq);
+	return (sq);
 }
