bce3e3cb94af9b97c957eca68b9a39d26736b81f
rcenamor <rcenamor@c1r1p2.hive.fi>
2020-01-24 16:01:20 +0200

    Draws straight and inclined lines. Added comments

--- srcs/main.c
+++ srcs/main.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rcenamor <rcenamor@student.hive.fi>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/24 15:41:18 by rcenamor          #+#    #+#             */
-/*   Updated: 2020/01/24 15:43:49 by rcenamor         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   main.c											 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: rcenamor <rcenamor@student.hive.fi>		+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2020/01/24 15:41:18 by rcenamor		  #+#	#+#			 */
+/*   Updated: 2020/01/24 15:43:49 by rcenamor		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "mlx.h"
@@ -60,6 +60,40 @@ int		main(void)
 	fdf.mlx = mlx_init();
 	fdf.win = mlx_new_window(fdf.mlx, 818, 500, "FDF");
 	paint_background(fdf.mlx, fdf.win);
+	draw_line(&fdf);
+	fdf.y0 = 200;
+	fdf.x1 = 300;
+	fdf.y1 = 200;
+	draw_line(&fdf);
+	fdf.x0 = 300;
+	fdf.y0 = 200;
+	fdf.x1 = 300;
+	fdf.y1 = 100;
+	draw_line(&fdf);
+	fdf.x0 = 300;
+	fdf.y0 = 100;
+	fdf.x1 = 100;
+	fdf.y1 = 100;
+	draw_line(&fdf);
+	fdf.x0 = 100;
+	fdf.y0 = 100;
+	fdf.x1 = 300;
+	fdf.y1 = 200;
+	draw_line(&fdf);
+	fdf.x0 = 300;
+	fdf.y0 = 100;
+	fdf.x1 = 100;
+	fdf.y1 = 200;
+	draw_line(&fdf);
+	fdf.x0 = 200;
+	fdf.y0 = 100;
+	fdf.x1 = 200;
+	fdf.y1 = 200;
+	draw_line(&fdf);
+	fdf.x0 = 100;
+	fdf.y0 = 150;
+	fdf.x1 = 300;
+	fdf.y1 = 150;
 	draw_line(&fdf);
 	//mlx_key_hook(fdf.win, key_press, fdf.mlx);
 	mlx_hook(fdf.win, 2, 0, key_press, fdf.mlx);
