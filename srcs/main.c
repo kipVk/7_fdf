7b69da21ab9a961c10d43e886fc7fe01233c3dd5
rcenamor <rcenamor@c2r1p2.hive.fi>
2020-02-20 11:49:30 +0200

    Added 2 perspectives, fix values for modifications for bigger size maps

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,45 @@
 /*   By: rcenamor <rcenamor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 16:04:10 by rcenamor          #+#    #+#             */
-/*   Updated: 2020/02/19 22:24:40 by rcenamor         ###   ########.fr       */
+/*   Updated: 2020/02/20 11:48:28 by rcenamor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	write_legend(t_fdf *fdf)
+{
+	mlx_string_put (fdf->mlx, fdf->win, 10, 10, PEAK_COLOR, " ^");
+	mlx_string_put (fdf->mlx, fdf->win, 10, 15, PEAK_COLOR, "< > Move");
+	mlx_string_put (fdf->mlx, fdf->win, 10, 20, PEAK_COLOR, " v");
+	mlx_string_put (fdf->mlx, fdf->win, 10, 40, PEAK_COLOR, "ESC Close");
+	mlx_string_put (fdf->mlx, fdf->win, 10, 60, PEAK_COLOR, "+ - Zoom");
+	mlx_string_put (fdf->mlx, fdf->win, 10, 80, PEAK_COLOR, "w s Height");
+	mlx_string_put (fdf->mlx, fdf->win, 10, 100, PEAK_COLOR, "q a Rotate");
+	mlx_string_put (fdf->mlx, fdf->win, 10, 120, PEAK_COLOR, "SPACE Persp");
+}
+
+void	ini_fdf(t_fdf *fdf)
+{
+	fdf->mlx = mlx_init();
+	fdf->win = mlx_new_window(fdf->mlx, WIN_W, WIN_H, WIN_NAME);
+	fdf->x0 = 0;
+	fdf->x1 = 0;
+	fdf->y0 = 0;
+	fdf->y1 = 0;
+	fdf->x = 0;
+	fdf->y = 0;
+	fdf->dx = 0;
+	fdf->dy = 0;
+	fdf->incx = 0;
+	fdf->incy = 0;
+	fdf->length = 0;
+	fdf->count_up = 0;
+	fdf->count_side = 0;
+	fdf->color = PEAK_COLOR;
+	fdf->inc_z = 3;
+	fdf->mul_x = 1;
+}
 
 int		key_press(int key, t_fdf *fdf)
 {
@@ -26,24 +60,48 @@ int		key_press(int key, t_fdf *fdf)
 		fdf->count_side -= SIDE_VALUE;
 	if (key == Q_K)
 	{
-		fdf->dist_x -= SIDE_VALUE;
-		fdf->dist_y -= SIDE_VALUE;
-		fdf->init_x += SIDE_VALUE;
-		fdf->init_y += SIDE_VALUE;
+		fdf->dist_x -= SIDE_VALUE * 0.2;
+		fdf->dist_y -= SIDE_VALUE * 0.2;
+		fdf->init_x += SIDE_VALUE * 0.2;
+		fdf->init_y += SIDE_VALUE * 0.2;
 	}
 	if (key == A_K)
 	{
-		fdf->dist_x += SIDE_VALUE;
-		fdf->dist_y += SIDE_VALUE;
-		fdf->init_x -= SIDE_VALUE;
-		fdf->init_y -= SIDE_VALUE;
+		fdf->dist_x += SIDE_VALUE * 0.2;
+		fdf->dist_y += SIDE_VALUE * 0.2;
+		fdf->init_x -= SIDE_VALUE * 0.2;
+		fdf->init_y -= SIDE_VALUE * 0.2;
 	}
 	if (key == W_K)
 		fdf->inc_z += 1;
 	if (key == S_K)
 		fdf->inc_z -= 1;
 	if (key == 49)
-		fdf->mul_x += 1;
+	{
+		fdf->persp++;
+		if (fdf->persp % 2 == 0)
+		{
+			fdf->dist_x = (WIN_W / ft_sqrt(pow((fdf->length * cos(M_PI / 3)), 2) + pow((fdf->lines * sin(M_PI / 6)), 2))) / 3;
+			fdf->dist_y = (WIN_H / ft_sqrt(pow((fdf->length * cos(M_PI / 3)), 2) + pow((fdf->lines * sin(M_PI / 6)), 2))) / 3;
+			fdf->init_x = (WIN_W / 3);
+			fdf->init_y = (WIN_H / 10);
+		}
+		else
+		{
+			fdf->dist_x = (WIN_W / fdf->length) * 0.7;
+			fdf->dist_y = (WIN_H / fdf->lines) * 0.7;
+			/* fdf->x0 = 0;
+			fdf->x1 = 0;
+			fdf->y0 = 0;
+			fdf->y1 = 0;
+			fdf->x = 0;
+			fdf->y = 0;
+			fdf->dx = 0;
+			fdf->dy = 0;
+			fdf->incx = 0;
+			fdf->incy = 0; */
+		}
+	}	
 	if (key == ZOOM_I)
 	{
 		fdf->dist_x *= 2;
@@ -71,47 +129,20 @@ int		mouse_press(int button, void *param)
 	return (0);
 }
 
-void	ini_fdf(t_fdf *fdf)
-{
-	fdf->mlx = mlx_init();
-	fdf->win = mlx_new_window(fdf->mlx, WIN_W, WIN_H, WIN_NAME);
-	fdf->x0 = 0;
-	fdf->x1 = 0;
-	fdf->y0 = 0;
-	fdf->y1 = 0;
-	fdf->x = 0;
-	fdf->y = 0;
-	fdf->dx = 0;
-	fdf->dy = 0;
-	fdf->incx = 0;
-	fdf->incy = 0;
-	fdf->length = 0;
-	fdf->count_up = 0;
-	fdf->count_side = 0;
-	fdf->color = PEAK_COLOR;
-	fdf->inc_z = 3;
-	fdf->mul_x = 1;
-}
-
-void	write_legend(t_fdf *fdf)
-{
-	mlx_string_put (fdf->mlx, fdf->win, 10, 10, PEAK_COLOR, " ^");
-	mlx_string_put (fdf->mlx, fdf->win, 10, 15, PEAK_COLOR, "< > Move");
-	mlx_string_put (fdf->mlx, fdf->win, 10, 20, PEAK_COLOR, " v");
-	mlx_string_put (fdf->mlx, fdf->win, 10, 40, PEAK_COLOR, "ESC Close");
-	mlx_string_put (fdf->mlx, fdf->win, 10, 60, PEAK_COLOR, "+ - Zoom");
-	mlx_string_put (fdf->mlx, fdf->win, 10, 80, PEAK_COLOR, "w s Height");
-	mlx_string_put (fdf->mlx, fdf->win, 10, 100, PEAK_COLOR, "q a Perspective");
-}
-
 void	redraw(t_fdf *fdf)
 {
 	mlx_clear_window(fdf->mlx, fdf->win);
-	//draw_thing(fdf);
-	draw_vgrid(fdf);
-	draw_hgrid(fdf);
+	if (fdf->persp % 2 == 0)
+	{
+		draw_v(fdf);
+		draw_h(fdf);
+	}
+	else
+	{
+		draw_vgrid(fdf);
+		draw_hgrid(fdf);
+	}
 	write_legend(fdf);
-
 }
 
 int		main(int ac, char **av)
